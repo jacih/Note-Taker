1f46d2170a933b39c475ec6da704e19c4964cd3a
Jacqueline Hodgson <jacqueline.e.hodgson@gmail.com>
2022-03-14 18:37:49 -0400

    more debugging + readme

---
+++ README.md
@@ -0,0 +1,38 @@
+
+# Note-Taker
+## ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg) 
+
+## Table of Contents
+- [Description](#description)
+- [Mock-Up](#mock-up)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## [Description](#table-of-contents)
+The Note Taker application is a simple express.js website for creating, saving and deleting notes for any user. A REST API has been implemented on the back-end to support the provided front end files, along with some helper scripts and routes to connect the front-end and back-end.
+
+## [Mock-Up](#table-of-contents)
+
+## [Installation](#table-of-contents)
+When a user visits the landing page, they are able to click a ‘Get Started’ button to be redirected to a notes webpage. The notes page contains an aside on the left of the page with existing notes populated in a list; each note has the option to delete through clicking of the trashcan favicon on the note’s row. New notes can be added through click of the plus favicon, which clears any text in the ‘Note Title’ and ‘Note Text’ input fields in the main section of the screen. When both a title and text have been added, a save favicon appears beside the plus favicon; upon click, the new note is added to the list of saved notes of the left of the screen. When a saved note is clicked, the content appears in the input field. Users can navigate back to the landing page by clicking the Note Taker link in the top left corner.
+
+## [Usage](#table-of-contents)
+This application is meant as an alternative to post-it notes, a day planner, relying on memory, or another note application.
+
+## [License](#table-of-contents)
+![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)
+[Apache 2.0 License](https://www.apache.org/licenses/LICENSE-2.0)
+
+## [Contribution Guidelines](#table-of-contents)
+I am currently not accepting contributions for this application.
+
+## [Tests](#table-of-contents)
+Currently, there are no test suites built.
+
+## [Questions](#table-of-contents)
+Contact me through [GitHub](https://github.com/jacih)
+Contact me through email at: [jacqueline.e.hodgson@gmail.com](mailto:jacqueline.e.hodgson@gmail.com)
--- server.js
+++ server.js
@@ -1,76 +1,76 @@
-const fs = require('fs');
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
-
 const uuid = require('./util/uuid');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 app.use(express.static('public'));
 
-//homepage route; takes user back to homepage
-app.get('*', (req,res) => {
+//homepage Routes
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-//note page route; takes user to notes page
-app.get('/notes', (req,res) => {
+app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-//note page route; reads json data in db.json file
-app.get('/api/notes', (req,res) => {
-  res.sendFile(path.join(__dirname, '/db/db.json'));
-})
-
-// note page route; creates new note object inside db.json file
-app.post('/api/notes', (req,res) => {
-  // access db.json file
-  fs.readFile(path.join(__dirname, '/db/db.json'), utf8, (err,res) => {
+//notes page Routes
+app.get('/api/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
     if (err) {
-      console.log(err);
+      console.error(err);
+    } else {
+      const notes = JSON.parse(data);
+      res.json(notes);
     }
-    //creates an array of notes; converted from json strings to javascript objects
-    const notes = JSON.parse(res);
-    //instantiate new object based on user input
-    const Note = {
-      id: uuid(),
-      title: req.body.title,
-      text: req.body.text
-    };
-    notes.push(Note);
-    //update db.json file; convert data back to json string
-    fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes, null, 2), (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('Note added successfully');
-      }
-    });
   });
 });
 
-//note page route; searches by id for note and deletes from db if it matches user request
-app.delete('/api/notes/:id', function(req,res) {
-
-  fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err,res) => {
+app.post('/api/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
     if (err) {
-      console.log(err);
+      console.error(err);
+    } else {
+      const savedNotes = JSON.parse(data);
+      const newNote = req.body;
+      newNote.id = uuid();
+      savedNotes.push(newNote);
+      fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(savedNotes), (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('New note added successfully');
+        }
+      });
     }
-    const notes = JSON.parse(res);
-    res.json(notes.splice(req.params.id, 1));
-    fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes, null, 2), (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('Note deleted successfully');
-      }
-    });
   });
 });
-  
-app.listen(PORT, () => console.log(`App listening at ${PORT}`));
+
+app.delete('/api/notes/:id', (req, res) => {
+  fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const savedNotes = JSON.parse(data);
+      const noteId = req.params.id;
+      const newNotes = savedNotes.filter(note => note.id !== noteId);
+      fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(newNotes), (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('Note deleted successfully');
+        }
+      });
+    }
+  });
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
